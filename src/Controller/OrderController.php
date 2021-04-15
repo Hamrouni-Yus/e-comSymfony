bbbaca440f6e347dc76703617c28a13e24ce71ec
Hamrouni-Yus <hamrounijosef@gmail.com>
2021-04-15 20:18:07 +0200

    Validation/Erreur de paiement de commande+Stripe

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -71,6 +71,8 @@ class OrderController extends AbstractController
 
             //Enregister ma commande Order()
             $order = new Order;
+            $reference = $date->format('dmY') . '-' . uniqid();
+            $order->setReference($reference);
             $order->setUser($this->getUser());
             $order->setCreatedAt($date);
             $order->setCarrierName($carriers->getName());
@@ -90,13 +92,13 @@ class OrderController extends AbstractController
                 $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
                 $this->entityManager->persist($orderDetails);
             }
-            $this->entityManager->flush();
 
+            $this->entityManager->flush();
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull(),
                 'carrier' => $carriers,
-                'delivery' => $delivery_content
-
+                'delivery' => $delivery_content,
+                'reference' => $order->getReference()
             ]);
         } else {
             return $this->redirectToRoute('cart');
---
+++ src/Controller/OrderSuccessController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class OrderSuccessController extends AbstractController
+{
+
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    /**
+     * @Route("/commande/merci/{stripeSessionId}", name="order_validate")
+     */
+    public function index($stripeSessionId): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneByStripeSessionId($stripeSessionId);
+
+        if (!$order || $order->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        if (!$order->getIsPaid()) {
+            $order->setIsPaid(1);
+            $this->entityManager->flush();
+        }
+
+
+        return $this->render('order_success/index.html.twig', [
+            'order' => $order
+        ]);
+    }
+}
